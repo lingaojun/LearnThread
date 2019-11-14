58168314ddf5625d4e2056bf8ed2428375610ec5
lingaojun <44394422+lingaojun@users.noreply.github.com>
2019-11-14 14:00:46 +0800

    NULL
    NULL

--- main.cpp
+++ main.cpp
@@ -2,30 +2,38 @@
 #include "thread"
 #include "mutex"
 #include "string"
+#include "condition_variable"
+#include <unistd.h>
 
 std::mutex Mutex;
-bool readyFlag;
+bool readyFlag = false;
 std::condition_variable readyCondition;
 
 void First(const std::string &s)
 {
-	std::lock_guard<std::mutex> Lock(Mutex);
+    std::unique_lock<std::mutex> Lock(Mutex);
+    std::cout << "First" << std::endl;
+    for(int i = 0; i < 3; i++)
+    {
+        sleep(1);
+        std::cout << "wait..." << std::endl;
+    }
 	readyFlag = true;
 	readyCondition.notify_one();
+    std::cout << "Fisrt Over" << std::endl;
 }
-void Second()   
+void Second()
 {
-	std::cout << "Hello World! " << std::endl;
 	std::unique_lock<std::mutex> Lk(Mutex);
+    std::cout << "Second" << std::endl;
 	readyCondition.wait(Lk, []{return readyFlag;});
+    std::cout << "Hello World" << std::endl;
 }
 
 int main()
 {
-	std::thread thread1(First,"Hello World");
-	std::thread thread2(Second);
-	std::cout << thread1.get_id() << std::endl; //线程ID 且每次运行程序值都会改变
-	std::cout << thread2.get_id() << std::endl;
+    std::thread thread2(Second);
+	std::thread thread1(First, "First");
 	thread1.join(); //调用它的线程即main线程会被block阻塞
 	thread2.join();
 
